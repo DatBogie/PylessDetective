24edea052ef6dc2fe4a838682e47f777b22e52fd
DatBogie <masterbuildermm2009@gmail.com>
2025-10-18 16:04:45 -0400

    Fixes, wip build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,12 @@
 @echo off
+python -m venv .venv
+.\.venv\Scripts\activate
+pip install -r requirements.txt
 pyinstaller --clean -ycF -n "PylessDetective" --add-data "maps/*.csv;maps/" main.py
+
+rmdir /s /q .\.venv
+
+python -m venv .venv
+.\.venv\Scripts\activate
+pip install -r requirements-gui.txt
+pyinstaller --clean -ywF -n "PylessDetectiveGui" --add-data "maps/*.csv;maps/" --add-data "main.py;main.py" gui.py
